342b6026198eb20aa93f16ba04323ad910d55f63
miraks <a@vldkn.net>
2015-09-29 22:29:46 +0300

    add dist

---
+++ dist/HelmetConstants.js
@@ -0,0 +1,25 @@
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var TAG_NAMES = {
+    BASE: "base",
+    META: "meta",
+    LINK: "link"
+};
+
+exports.TAG_NAMES = TAG_NAMES;
+var TAG_PROPERTIES = {
+    NAME: "name",
+    CHARSET: "charset",
+    HTTPEQUIV: "http-equiv",
+    REL: "rel",
+    HREF: "href",
+    PROPERTY: "property"
+};
+
+exports.TAG_PROPERTIES = TAG_PROPERTIES;
+var REACT_TAG_MAP = {
+    "charset": "charSet",
+    "http-equiv": "httpEquiv"
+};
+exports.REACT_TAG_MAP = REACT_TAG_MAP;
